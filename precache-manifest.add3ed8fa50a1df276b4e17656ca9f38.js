0993c7f0c727599bd4fae8bbba5f132c494d6062
ryanarine <47193209+ryanarine@users.noreply.github.com>
2019-09-25 18:17:47 -0400

    Updates

---
+++ precache-manifest.add3ed8fa50a1df276b4e17656ca9f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddff36ddba12ccf5835c10b4f690be86",
+    "url": "/Image-Processor/index.html"
+  },
+  {
+    "revision": "f2eee1edc6d2918959ad",
+    "url": "/Image-Processor/static/css/main.746b69f4.chunk.css"
+  },
+  {
+    "revision": "5b811bd876d02d9c844f",
+    "url": "/Image-Processor/static/js/2.7c7d4abd.chunk.js"
+  },
+  {
+    "revision": "f2eee1edc6d2918959ad",
+    "url": "/Image-Processor/static/js/main.bc82609a.chunk.js"
+  },
+  {
+    "revision": "a78fbe28b2a7ade8e01f",
+    "url": "/Image-Processor/static/js/runtime-main.41475bd8.js"
+  },
+  {
+    "revision": "af03c4b1d3feead6a94111daf85ea18f",
+    "url": "/Image-Processor/static/media/transparent.af03c4b1.jpg"
+  }
+]);
