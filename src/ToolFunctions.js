eda1c93b460f59f1e1cb29745171c2e43b4c6736
ryanarine <47193209+ryanarine@users.noreply.github.com>
2019-09-29 21:05:40 -0400

    Performance Fix
    The section detection function was adding pixels to the frontier that
    had already been added before

--- src/ToolFunctions.js
+++ src/ToolFunctions.js
@@ -30,10 +30,11 @@ export function changeImgData(file1, file2, oldVal, newVal, tolerance) {
   -Returns an array of pixel indices that corresponds to a section in Image originating at pixel with the given tolerance range
 */
 export function getSection(Image, pixel, tolerance) {
+  let start = new Date();
   let [width, height, data] = [Image.width, Image.height, Image.data];
   // Adjacent offsets of pixels starting from 0 deg bearing going clockwise
   let adjacent = [[0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
-  // visited[i] indicates whether pixel i has been visited or not
+  // visited[i] indicates whether pixel i has been added to the frontier before
   let visited = new Array(width * height);
   // The array of pixels to return
   let section = [];
@@ -41,14 +42,13 @@ export function getSection(Image, pixel, tolerance) {
   let [x, y] = [pixel[4], pixel[5]];
   // The array of pixels that need to be checked if they belong to the section
   let frontier = [[x, y]];
+  visited[y * width + x] = true;
   while (frontier.length !== 0) {
     [x, y] = frontier.pop();
     // Check if pixel has valid coordinates
     if (x < 0 || x > width || y < 0 || y > height) {
       continue;
     }
-    // Mark the pixel visited
-    visited[y * width + x] = true;
     // Get the colour of the pixel
     let colour = [];
     for (let offset = 0; offset < 4; offset++) {
@@ -60,8 +60,9 @@ export function getSection(Image, pixel, tolerance) {
       section.push(y * width * 4 + x * 4);
       // Add adjacent pixels to frontier
       adjacent.forEach(adj => {
-        // If they have not been visited before
+        // If they have not been added before
         if (!visited[(y + adj[1]) * width + x + adj[0]]) {
+          visited[(y + adj[1]) * width + x + adj[0]] = true;
           frontier.push([x + adj[0], y + adj[1]]);
         }
       });
